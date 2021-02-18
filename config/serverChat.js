02f3e2ea5638348146240343c484e659a046f7b1
Bruno4745 <bruno_4745@hotmail.com>
2021-02-17 21:58:30 -0300

    teste

--- config/serverChat.js
+++ config/serverChat.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, '../public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.get('/chat', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index.html');
 });
 

70893f322e99047401c8cb5db86a32b5293d4a10
Bruno4745 <bruno_4745@hotmail.com>
2021-03-03 18:41:10 -0300

    Melhorando o chat

--- index.js
+++ index.js
@@ -14,31 +14,31 @@ app.get('/',(req,res)=>{
 })
 
 server=app.listen(port,function(){
-  console.log(`Server started on port ${port} ...`);
+  console.log(`Servidor rodando na porta ${port}`);
 })
 
 const io = require('socket.io')(server);
 
 io.on('connection',(socket)=>{
-  socket.username='Anonymous'
+  socket.username='Anônimo'
   var name=socket.username
-  socket.broadcast.emit('joined_room',{name:socket.username+ ' joined the room'});
-  console.log('New User Connected with name:'+ socket.username);
+  socket.broadcast.emit('joined_room',{name:socket.username+ ' se conectou no chat'});
+  console.log('Novo usuário conectado: '+ socket.username);
 
   socket.on('new_msg',(data)=>{
     console.log(data);
     socket.broadcast.emit('new_msg_all',data)
   })
 
-  socket.on('change_name',(data)=>{
-    io.sockets.emit('joined_room',{name:socket.username+ ' changed into '+data.username});
-    socket.username=data.username;
-    name=socket.username
-    console.log('UserName Changed to :'+socket.username);
-  })
+  // socket.on('change_name',(data)=>{
+  //   io.sockets.emit('joined_room',{name:socket.username+ ' changed into '+data.username});
+  //   socket.username=data.username;
+  //   name=socket.username
+  //   console.log('UserName Changed to :'+socket.username);
+  // })
 
   socket.on('disconnect',(socket)=>{
-    console.log('disconnect');
-    io.sockets.emit('joined_room',{name:name+ ' has left the room'});
+    console.log('Usuário desconectado: '+ name);
+    io.sockets.emit('joined_room',{name:name+ ' se desconectou do chat'});
   })
 })
